0344d34d6799c9d731dc851b8e0cd5b1a708d8a1
Mariusz Kozakowski <11mariom+wordpress@gmail.com>
2013-08-24 10:10:45 +0200

    lua/todo: creating file, add help

--- lua/todo/todo.lua
+++ lua/todo/todo.lua
@@ -2,18 +2,26 @@
 tasks={}
 y=" \27[32m✔\27[0m "
 n=" \27[31m✗\27[0m "
+file= os.getenv("HOME") .. "/task"
 
 function read()
-   for line in io.lines( 'task' ) do
+   local f = io.open ( file, 'r' )
+   if f == nil then
+      f = io.open ( file, 'w' )
+   end
+   f:close()
+
+   for line in io.lines( file ) do
       table.insert( tasks, line )
    end
 end
 
 function save()
-   local file = io.open ( 'task', 'w' )
+   local f = io.open ( file, 'w' )
    for i,v in pairs( tasks ) do
-      file:write( v .. "\n")
+      f:write( v .. "\n" )
    end
+   f:close()
 end
 
 function print_t( opt )
@@ -40,6 +48,15 @@ if     o == "ls" then print_t( arg[2] )
 elseif o == "rm" then table.remove( tasks, n )
 elseif o == "check" or o == "undo" then toggle( tonumber( arg[2] ) )
 elseif o == "clear" then tasks = {}
+elseif o == nil then print [[
+Usage: [options] task
+Options:
+ ls     - list tasks
+ rm     - remove nth task
+ check  - mark as done
+ undo   - mark to do
+ clear  - remove all tasks
+]]
 else   table.insert( tasks, "-" .. o )
 end
 save()
